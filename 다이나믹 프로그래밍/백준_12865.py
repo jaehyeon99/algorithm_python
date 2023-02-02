d5f3f9190c6d83384955b7db28fbac05d41d42c3
jaehyeon99 <janhyun99@naver.com>
2023-02-02 19:42:08 +0900

    23.02.01

---
+++ 다이나믹 프로그래밍/백준_12865.py
@@ -0,0 +1,17 @@
+n,k = map(int,input().split())
+arr = [[0,0]]
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+
+d = [[0 for _ in range(k+1)] for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1,k+1):
+        weight = arr[i][0]
+        value = arr[i][1]
+        if j < weight:
+            d[i][j] = d[i-1][j]
+        else:
+            d[i][j] = max(d[i-1][j],d[i-1][j-weight]+value)
+
+print(d[n][k])
