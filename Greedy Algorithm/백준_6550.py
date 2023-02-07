90b27f932a0a2eb822a577ca01f5f419aa87ae04
jaehyeon99 <janhyun99@naver.com>
2023-02-08 00:34:28 +0900

    23.02.07

---
+++ Greedy Algorithm/백준_6550.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+while True:
+    try:
+        s, t = input().split()
+        queue = deque(list(s))
+
+        for c in t:
+            if queue and c == queue[0]:
+                queue.popleft()
+
+        if queue:
+            print('No')
+        else:
+            print('Yes')
+    except:
+        break
